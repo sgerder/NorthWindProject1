421922b6ee0f1e379dabeb37c8197ae838f2cf80
Jdeichmann <jdeichmann@protonmail.com>
2021-10-26 16:30:04 +0200

    Added Order and Orderdetails

---
+++ EfEx/Domain/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EfEx.Domain
+{
+    public class Order
+    {
+        //[Table("Orders")]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string ShipName { get; set; }
+        public string ShipCity { get; set; }
+        public List<OrderDetails> OrderDetails { get; set; }
+    }
+}
--- EfEx/Domain/Product.cs
+++ EfEx/Domain/Product.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace EfEx.Domain
 {
+    //[Table("products")]
     public class Product
     {
         public int Id { get; set; }
