b86ea0f6d1e7897aabb39245755effa421e52aa1
Jonas <jdeichmann@protonmail.com>
2021-10-27 11:06:17 +0200

    mapped order and orderdetails. Also added gitattributes and gitignore

--- EfEx/Domain/OrderDetails.cs
+++ EfEx/Domain/OrderDetails.cs
@@ -4,9 +4,7 @@
     {
         //[Table("orderdetails")]
         public int OrderId {get; set; }
-        public Order Order { get; set; }
         public int ProductId { get; set; }
-        public Product Product { get; set; }
         public int UnitPrice { get; set; }
         public int Quantity { get; set; }
         public int Discount { get; set; }
--- EfEx/NorthwindContext.cs
+++ EfEx/NorthwindContext.cs
@@ -9,6 +9,10 @@ namespace EfEx
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        
+        public DbSet<Order> Orders { get; set; }
+        
+        public DbSet<OrderDetails> OrderDetails { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -16,7 +20,7 @@ namespace EfEx
 
             optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
             optionsBuilder.EnableSensitiveDataLogging();
-            optionsBuilder.UseNpgsql("host=localhost;db=northwind;uid=bulskov;pwd=henrik");
+            optionsBuilder.UseNpgsql("host=localhost;db=northwind;uid=postgres;pwd=Emma130395!");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -27,13 +31,27 @@ namespace EfEx
             modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
             modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
             modelBuilder.Entity<Category>().Property(x => x.Description).HasColumnName("description");
-
-
+            
+            
             modelBuilder.Entity<Product>().ToTable("products");
             modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("productid");
             modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("productname");
             modelBuilder.Entity<Product>().Property(x => x.CategoryId).HasColumnName("categoryid");
 
+            
+            modelBuilder.Entity<Order>().ToTable("orders");
+            modelBuilder.Entity<Order>().Property(x => x.Id).HasColumnName("orderid");
+            modelBuilder.Entity<Order>().Property(x => x.Date).HasColumnName("orderdate");
+            modelBuilder.Entity<Order>().Property(x => x.ShipName).HasColumnName("shipname");
+            modelBuilder.Entity<Order>().Property(x => x.ShipCity).HasColumnName("shipcity");
+            
+            modelBuilder.Entity<OrderDetails>().ToTable("orderdetails");
+            modelBuilder.Entity<OrderDetails>().Property(x => x.OrderId).HasColumnName("orderid");
+            modelBuilder.Entity<OrderDetails>().Property(x => x.ProductId).HasColumnName("productid");
+            modelBuilder.Entity<OrderDetails>().Property(x => x.UnitPrice).HasColumnName("unitprice");
+            modelBuilder.Entity<OrderDetails>().Property(x => x.Quantity).HasColumnName("quantity");
+            modelBuilder.Entity<OrderDetails>().Property(x => x.Discount).HasColumnName("discount");
+            
         }
     }
 }
