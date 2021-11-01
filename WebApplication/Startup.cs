e012d04a3a8398ec996174eae434dc2dd531eb50
Burnirr <andrei.sv97@yahoo.com>
2021-11-01 15:05:37 +0100

    The api/categories shows in the web browser.

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -34,7 +34,9 @@ namespace WebApplication
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+               
             });
         }
     }
